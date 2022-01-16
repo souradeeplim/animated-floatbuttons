4395a55c3dcdb179e1bc2ac06135149d9f395d3a
souradeeplim <souradeep.modal@lifeinmba.com>
2022-01-16 15:51:40 +0530

    .

--- lib/widgets/animated_floating_action_button.dart
+++ lib/widgets/animated_floating_action_button.dart
@@ -130,6 +130,7 @@ class AnimatedFloatingActionButtonState
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: _buildFABs(),
